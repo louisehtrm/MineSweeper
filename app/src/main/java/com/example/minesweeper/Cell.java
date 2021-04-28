c6535e9d6205d7a07ca58a0e6b3d8650b8835741
Louise Hartmann <louise.hartmann@epita.fr>
2021-04-28 15:30:48 +0100

    Milestone 4: add the random mines and change the aspect of the cell when a mine is found

--- app/src/main/java/com/example/minesweeper/Cell.java
+++ app/src/main/java/com/example/minesweeper/Cell.java
@@ -3,9 +3,11 @@ package com.example.minesweeper;
 public class Cell {
 
     boolean unCovered;
+    boolean mine;
 
     public void initCell() {
         unCovered = false;
+        mine = false;
     }
 
 }
--- app/src/main/java/com/example/minesweeper/GameView.java
+++ app/src/main/java/com/example/minesweeper/GameView.java
@@ -14,6 +14,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import java.util.Random;
+
 public class GameView extends View {
 
     //initialize attributes
@@ -22,6 +24,8 @@ public class GameView extends View {
     Rect square;
     //use for color and styling
     private Paint rectPaint;
+    //use to add text
+    private TextPaint rectText;
 
     int sideLength;
 
@@ -52,11 +56,35 @@ public class GameView extends View {
         setBackgroundColor(Color.WHITE);
 
         //initialize the matrix
+        initCell();
+
+        //place the mines
+        randomMine();
+
+    }
+
+    //initialize the matrix
+    public void initCell() {
         for (int i = 0; i < 10; i++) {
             for(int j = 0; j < 10; j++) {
                 Cell c = new Cell();
                 c.initCell();
                 cell[i][j] = c;
+            }
+        }
+    }
+
+    //choose 20 mines randomly
+    public void randomMine() {
+        Random rand = new Random();
+        int i, j;
+        int k = 0;
+        while(k < 20) {
+            i = rand.nextInt(10);
+            j = rand.nextInt(10);
+            if(!cell[i][j].mine) {
+                cell[i][j].mine = true;
+                k++;
             }
         }
     }
@@ -99,15 +127,16 @@ public class GameView extends View {
         //Bounds of squares to be drawn (10*10)
         int rectBounds = contentWidth/10;
 
-        //Side length of the square (10 = distance between two squares)
-        sideLength = rectBounds - 10 ;
+        //Side length of the square (2 = distance between two squares)
+        sideLength = rectBounds - 2 ;
 
         //Paint instance for drawing the squares
         rectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        //rectPaint.setColor(Color.BLACK);
 
         //Create a rect which is actually a square
         square = new Rect(10,10, sideLength, sideLength);
+
+        rectText = new TextPaint();
 
         //Draw the squares
         //draw ten lines of ten squares
@@ -122,13 +151,31 @@ public class GameView extends View {
                 canvas.translate(j * rectBounds, i * rectBounds);
 
                 //check the state for the color
-                if(cell[i][j].unCovered)
-                    rectPaint.setColor(Color.GRAY);
-                else
+                if(!cell[i][j].unCovered) {
                     rectPaint.setColor(Color.BLACK);
 
-                //Draw the square
-                canvas.drawRect(square, rectPaint);
+                    //Draw the square
+                    canvas.drawRect(square, rectPaint);
+                }
+                else if(cell[i][j].mine) {
+                    rectPaint.setColor(Color.RED);
+                    rectText.setTextSize(70);
+                    rectText.setStyle(TextPaint.Style.FILL);
+                    rectText.setColor(Color.BLACK);
+
+                    //Draw the square
+                    canvas.drawRect(square, rectPaint);
+
+                    //Draw the text
+                    canvas.drawText("M", j + sideLength * 1/4, i + sideLength * 3/4, rectText);
+
+                }
+                else {
+                    rectPaint.setColor(Color.GRAY);
+
+                    //Draw the square
+                    canvas.drawRect(square, rectPaint);
+                }
 
                 //Restore, back to the origin
                 canvas.restore();
