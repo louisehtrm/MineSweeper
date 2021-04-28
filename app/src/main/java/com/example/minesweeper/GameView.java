b0bc1e50c037acb2c7149c86be4687c8d179218d
Louise Hartmann <louise.hartmann@epita.fr>
2021-04-28 11:31:22 +0100

    Milestone 2: new adjustments of Buttons and TextViews and creation of the cells for the game (10*10)

--- app/src/main/java/com/example/minesweeper/GameView.java
+++ app/src/main/java/com/example/minesweeper/GameView.java
@@ -13,6 +13,10 @@ import androidx.annotation.Nullable;
 
 public class GameView extends View {
 
+    //initialize attributes
+    private Paint rectPaint;
+    Rect square;
+
     //the four constructors
 
     public GameView(Context context) {
@@ -38,11 +42,55 @@ public class GameView extends View {
 
     //initialization method
     private void init(AttributeSet attrs) {
+        //Set the background color.
+        setBackgroundColor(Color.WHITE);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        // allocations per draw cycle.
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+
+        //Bounds of squares to be drawn (10*10)
+        int rectBounds = contentWidth/10;
+
+        //Side length of the square (5 = distance between two squares)
+        int sideLength = rectBounds -5 ;
+
+        //Paint instance for drawing the squares.
+        rectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        rectPaint.setColor(Color.BLACK);
+
+        //Create a rect which is actually a square.
+        square = new Rect(10,10, sideLength, sideLength);
+
+        //Draw the squares
+        //draw ten lines of ten squares
+        for(int i = 0; i < 10; i++) {
+            //draw a line of square at the i index
+            for (int j = 0; j < 10; j++) {
+
+                //Preliminary save of the drawing origin to the stack.
+                canvas.save();
+
+                //Translate to draw a row of squares. Firs will be at (0,0).
+                canvas.translate(j * rectBounds, i * rectBounds);
+                //Draw it.
+                canvas.drawRect(square, rectPaint);
+
+                //Restore. Back to the origin.
+                canvas.restore();
+
+            }
+        }
     }
 }
 
